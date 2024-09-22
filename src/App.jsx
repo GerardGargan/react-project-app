faf8962164535cd1e9dfa54bc627aa3d98c009f1
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-09-22 21:01:26 +0100

    handle new project

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,32 @@ import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
 
-  const [openNewProject, setOpenNewProject] = useState(false);
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects:[]
+  });
 
-  function createNewProject() {
-    setOpenNewProject(true);
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    });
   }
+
+  let content;
+  if(projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if(projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onPressNewProject={handleStartAddProject} />
+  }
+
   
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar onPressNewProject={createNewProject} />
-      {openNewProject ? <NewProject /> : <NoProjectSelected />}
+      <Sidebar onPressNewProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
