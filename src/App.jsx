d1da00b13f480f50104e165a57759b98f29c330f
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-09-22 19:22:34 +0100

    add new project component and handle state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import Sidebar from "./components/Sidebar";
-
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+
+  const [openNewProject, setOpenNewProject] = useState(false);
+
+  function createNewProject() {
+    setOpenNewProject(true);
+  }
   
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NewProject />
+      <Sidebar onPressNewProject={createNewProject} />
+      {openNewProject ? <NewProject /> : <NoProjectSelected />}
     </main>
   );
 }
